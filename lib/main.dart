1f6fdf65ca8d221c1c9845dc973dd88e540e5925
Adam Smaka <adam@smaka.dev>
2022-02-01 11:26:24 +0100

    Add initial UserItemWidget

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,66 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: const [
+          UserItemWidget(
+            avatarURL: 'https://randomuser.me/api/portraits/men/3.jpg',
+            name: 'Piotr Obdarowicz',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class UserItemWidget extends StatelessWidget {
+  const UserItemWidget({
+    Key? key,
+    required this.avatarURL,
+    required this.name,
+  }) : super(key: key);
+
+  final String avatarURL;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      color: Colors.grey.shade200,
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              avatarURL,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Text(name),
+          ),
+        ],
+      ),
     );
   }
 }
