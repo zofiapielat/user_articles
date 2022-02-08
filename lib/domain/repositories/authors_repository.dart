c547abbf391d5d58c505bd8fd3292d67dc775982
Adam Smaka <adam@smaka.dev>
2022-02-08 13:32:29 +0100

    Implement auto_registering injectable

--- lib/domain/repositories/authors_repository.dart
+++ lib/domain/repositories/authors_repository.dart
@@ -1,8 +1,6 @@
-import 'package:injectable/injectable.dart';
 import 'package:user_articles/data/remote_data_sources/authors_remote_data_source.dart';
 import 'package:user_articles/domain/models/author_model.dart';
 
-@injectable
 class AuthorsRepository {
   AuthorsRepository({required this.remoteDataSource});
 
