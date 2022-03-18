a5aa8682961c304b895e36888ae24785472097e4
Adam Smaka <adam@smaka.dev>
2022-03-18 18:19:13 +0100

    Add vsc lanch.json

--- lib/main_release.dart
+++ lib/main_release.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:user_articles/app/app.dart';
+import 'package:user_articles/app/core/config.dart';
 import 'package:user_articles/app/injection_container.dart';
 
 void main() {
+  Config.appFlavor = Flavor.release;
   configureDependencies();
   runApp(const MyApp());
 }
