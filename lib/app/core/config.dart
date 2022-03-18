b0a63d98a1fda20b8f3e6cc71494a739c2dc9b44
Adam Smaka <adam@smaka.dev>
2022-03-18 18:07:15 +0100

    Create config file

---
+++ lib/app/core/config.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+enum Flavor {
+  development,
+  release,
+}
+
+class Config {
+  static Flavor appFlavor = Flavor.development;
+
+  static String get helloMessage {
+    switch (appFlavor) {
+      case Flavor.release:
+        return 'release';
+      case Flavor.development:
+      default:
+        return 'development';
+    }
+  }
+
+  static String get baseUrl {
+    switch (appFlavor) {
+      case Flavor.release:
+        return 'https://my-json-server.typicode.com/adamsmaka/json-demo';
+      case Flavor.development:
+        return 'https://my-json-server.typicode.com/adamsmaka/json-demo';
+    }
+  }
+
+  static Icon get helloIcon {
+    switch (appFlavor) {
+      case Flavor.release:
+        return const Icon(Icons.new_releases);
+      case Flavor.development:
+      default:
+        return const Icon(Icons.developer_mode);
+    }
+  }
+}
