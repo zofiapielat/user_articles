2d4cf51a188f1636dd6133789a6c55545a475734
ZofiaPielat <z.pielat@gmail.com>
2023-10-03 17:37:49 +0200

    Remove unnecessary hardcoded data from data sources

--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -1,24 +1,5 @@
 import 'package:dio/dio.dart';
 
-class AuthorsMockedDataSource {
-  Future<List<Map<String, dynamic>>?> getAuthors() async {
-    return [
-      {
-        'id': 1,
-        'picture': 'https://randomuser.me/api/portraits/men/57.jpg',
-        'first_name': 'Piotr',
-        'last_name': 'Obdarowicz',
-      },
-      {
-        'id': 2,
-        'picture': 'https://randomuser.me/api/portraits/men/58.jpg',
-        'first_name': 'Radosław',
-        'last_name': 'Gdański',
-      },
-    ];
-  }
-}
-
 class AuthorsRemoteDioDataSource {
   Future<List<Map<String, dynamic>>?> getAuthors() async {
     final response = await Dio().get<List<dynamic>>(
