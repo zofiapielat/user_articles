3ba083f823217ba4bfeb9f5ee6879cd864bacf4d
ZofiaPielat <z.pielat@gmail.com>
2023-10-05 14:12:47 +0200

    Add content to article details page

--- lib/article_details/article_details_page/article_details_page.dart
+++ lib/article_details/article_details_page/article_details_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:user_articles/app/injection_container.dart';
-import 'package:user_articles/article_details/cubit/article_details_cubit.dart';
 import 'package:user_articles/domain/models/article_model.dart';
 
 class ArticleDetailsPage extends StatelessWidget {
@@ -15,56 +12,36 @@ class ArticleDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('tytuł'),
+          title: Text(details.title),
         ),
-        body: BlocProvider<ArticleDetailsCubit>(
-          create: (context) => getIt()
-            ..fetchArticleData(
-              id: details.id,
-            ),
-          child: const Column(
-            children: [
-              _ArticleDetailsWidget(),
-            ],
-          ),
-        ));
-  }
-}
-
-class _ArticleDetailsWidget extends StatelessWidget {
-  const _ArticleDetailsWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Image(
-          image: AssetImage(''),
-          height: 50,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Padding(
+        body: Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: 20,
             vertical: 20,
           ),
-          child: Container(
-            color: Colors.black12,
-            padding: const EdgeInsets.symmetric(
-              vertical: 20,
-              horizontal: 20,
-            ),
-            child: const Expanded(
-              child: Text(
-                  'Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Nazywał się możemy na wieś rozweselić. Tadeusz Telimenie, Asesor Krajczance a często bez żadnych ozdób, ale szerzej niż myśliwi młodzi tak było jeszcze dobrze zachowana sklepienie całe zaczerwienione, jak on znowu w kuca. Obaczcież, co dzień postrzegam, jak czas i zdrowie. Nazywał się zdawał małpą lub wymowy uczyć się tłocz i tam ma szkół uczących żyć z dom nikt lepiej nad błękitnym Niemnem rozciągnionych. Do zobaczenia! tak wedle dzisiejszej mody jeździł Hreczecha. Tu śmiech młodzieży mowę Wojskiego zagłuszył.'),
+          child: Center(
+            child: Column(
+              children: [
+                Image(
+                  image: NetworkImage(details.picture),
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  color: Colors.black12,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 20,
+                  ),
+                  child: Expanded(
+                    child: Text(details.content),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ],
-    );
+        ));
   }
 }
